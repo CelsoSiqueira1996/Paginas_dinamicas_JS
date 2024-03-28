e4d31b6e4dd42c728e1614a8e8c2b80f25d5b561
Celso <celso.jacinto.siqueira.neto@gmail.com>
2024-03-28 16:21:46 -0300

    Adicionada nova tecla ('Play')

--- main.js
+++ main.js
@@ -3,21 +3,52 @@ function tocaSom(seletorAudio) {
 
     if(elemento && elemento.localName === 'audio') {
         elemento.play();
+        sequenciaSons.push(elemento);
     } else {
         alert('Elemento não encontrado ou seletor inválido');
     }
 
 }
 
+function tocarMusica() {
+    if(intervaloMusica === null) {
+        intervaloMusica = setInterval(musica, 300);
+    } else {
+        clearInterval(intervaloMusica);
+        intervaloMusica = null;
+    }
+}
+
+function musica() {
+    if(contador < sequenciaSons.length) {
+        sequenciaSons[contador].play();
+        contador++;
+    } else {
+        clearInterval(intervaloMusica);
+        intervaloMusica = null;
+        contador = 0;
+        sequenciaSons = [];
+    }
+}
+
+let intervaloMusica = null;
+let contador = 0;
 const listaDeTeclas = document.querySelectorAll('.tecla');
+let sequenciaSons = [];
 
 listaDeTeclas.forEach((tecla) => {
 
     const instrumento = tecla.classList[1];
     const idElementoAudio = '#som_' + instrumento;
 
-    tecla.onclick = () => {
-        tocaSom(idElementoAudio); 
+    if(tecla.classList.contains('tecla_play')) {
+        tecla.onclick = () => {
+            tocarMusica();
+        }
+    } else {
+        tecla.onclick = () => {
+            tocaSom(idElementoAudio); 
+        }
     }
 
     tecla.onkeydown = (evento) => {
